a2d66c9487dac3216e17d77471ac26f086b1fe47
Louis Rollins <louisrollins01@gmail.com>
2019-06-25 18:48:20 -0400

    spotify api call returns data and name of song, not artists

--- keys.js
+++ keys.js
@@ -7,4 +7,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
--- liri.js
+++ liri.js
@@ -2,23 +2,49 @@ require('dotenv').config()
 
 var keys = require('./keys.js')
 
+var searchQuery = []
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2]
-switch (command){
+var command2 = process.argv[3]
+var multiCommand = function(){
+    for (i=3; i<process.argv.length; i++){
+        searchQuery.push(process.argv[i])
+        console.log(searchQuery)
+    }
+}
+multiCommand()
+
+switch (command) {
     case 'concert-this':
 
-    break;
+        break;
 
     case 'spotify-this-song':
 
-    break;
+        spotify.search({
+            type: 'track',
+            limit: 5,
+            query: searchQuery
+        }, function(err, data){
+            if (err){
+                console.log(err)
+            }
+            // console.log(data.tracks.items)
+            for (i=0; i < data.tracks.items.length; i++){
+                console.log(data.tracks.items[i].name)
+                var artist = data.tracks.items.artists
+                console.log(Object.keys(artist)[3])
+            }
+        })
+        break;
 
     case 'movie-this':
 
-    break;
+        break;
 
     case 'do-what-it-says':
 
-    break;
+        break;
 }
